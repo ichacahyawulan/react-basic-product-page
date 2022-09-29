a30022b3efcc1192b3fa9854d4f7f0fadcb47aa2
Icha Cahya Wulan <ichacahyawulan@gmail.com>
2022-09-29 16:55:58 +0700

    create product page

---
+++ src/Topbar/Topbar.js
@@ -0,0 +1,13 @@
+import classes from './Topbar.module.css'
+
+const Topbar = (props) => {
+  return(
+    <header>
+      <nav className={classes.Topbar}>
+        <img src="https://i.dlpng.com/static/png/197684_preview.png" alt="Amazon Logo" />
+      </nav>
+    </header>
+  );
+}
+
+export default Topbar
